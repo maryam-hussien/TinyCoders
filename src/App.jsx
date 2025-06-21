de32b6f3eaa471be39f558d06080222e9148c11f
maryam-hussien <maryamhussien122@gmail.com>
2025-06-21 20:05:58 +0300

    fonts edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import "./App.css";
 function App() {
   return (
     <>
-     <div style={{ backgroundColor: 'var(--popy)' ,fontFamily:'var(--font-Mystery-Quest)'}} className="text-3xl font-bold text-white p-2">
+     <div style={{ backgroundColor: 'var(--popy)' }} className="text-3xl font-bold text-white p-2 mysteryFont">
         Custom color: Popy and font Mystery-Quest
       </div>
 
-      <div style={{ backgroundColor: 'var(--mango)',fontFamily:'var(--font-Butterfly-Kids)'}} className="text-3xl font-bold text-white p-2">
+      <div style={{ backgroundColor: 'var(--mango)'}} className="text-3xl font-bold text-white p-2 butterflyFont">
         Custom color: Mango and font Butterfly-Kids
       </div>
-      <div style={{ backgroundColor: 'var(--forest)',fontFamily:'var(--Sour-Gummy)'}} className="text-3xl font-bold text-white p-2">
+      <div style={{ backgroundColor: 'var(--forest)'}} className="text-3xl font-bold text-white p-2 sourGummyFont">
         Custom color: forest and font Sour-Gummy
       </div>
-      <div style={{ backgroundColor: 'var(--lavender)' ,fontFamily:'var(--Roboto)'}} className="text-3xl font-bold text-white p-2">
+      <div style={{ backgroundColor: 'var(--lavender)'}} className="text-3xl font-bold text-white p-2 robotoFont">
         Custom color: lavender and font Roboto
       </div>
       <div style={{ backgroundColor: 'var(--babyBlue)'}} className="text-3xl font-bold text-white p-2">
