b9f2af1e8e52e33687910be35db0137ea4b4874e
maryam-hussien <maryamhussien122@gmail.com>
2025-06-21 19:47:42 +0300

    colrs and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
-import './App.css'
-
+import "./App.css";
 function App() {
-
   return (
     <>
-      <div className="text-3xl font-bold text-blue-500">
-      Tailwind is working!
-    </div>
+     <div style={{ backgroundColor: 'var(--popy)' ,fontFamily:'var(--font-Mystery-Quest)'}} className="text-3xl font-bold text-white p-2">
+        Custom color: Popy and font Mystery-Quest
+      </div>
+
+      <div style={{ backgroundColor: 'var(--mango)',fontFamily:'var(--font-Butterfly-Kids)'}} className="text-3xl font-bold text-white p-2">
+        Custom color: Mango and font Butterfly-Kids
+      </div>
+      <div style={{ backgroundColor: 'var(--forest)',fontFamily:'var(--Sour-Gummy)'}} className="text-3xl font-bold text-white p-2">
+        Custom color: forest and font Sour-Gummy
+      </div>
+      <div style={{ backgroundColor: 'var(--lavender)' ,fontFamily:'var(--Roboto)'}} className="text-3xl font-bold text-white p-2">
+        Custom color: lavender and font Roboto
+      </div>
+      <div style={{ backgroundColor: 'var(--babyBlue)'}} className="text-3xl font-bold text-white p-2">
+        Custom color: babyBlue no font
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
