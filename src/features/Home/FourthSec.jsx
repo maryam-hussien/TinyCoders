1ef147df86916e4f6ba9fe68341814ebab88671b
maryam-hussien <maryamhussien122@gmail.com>
2025-06-24 19:45:39 +0300

    edits

--- src/features/Home/FourthSec.jsx
+++ src/features/Home/FourthSec.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FourthSec = () => {
   return (
-    <div className="w-[85%] md:w-[80%] mx-auto   sourGummyFont bg-[#e9e8e8f0] rounded-4xl p-6 shadow-2xl h-100 mb-5">Points and challanges</div>
+    <div className="w-[85%]  mx-auto   sourGummyFont bg-[#e9e8e8f0] rounded-4xl p-4 shadow-2xl h-100 mb-8">Points and challanges</div>
   )
 }
 
